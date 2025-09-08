6be3056e4bd297c841767806dfe63ec4603b5931
NaijunWang <wnj123e@gmail.com>
2025-09-07 20:31:22 -0400

    function: finished hw0

--- CSVCalculatorHandler.py
+++ CSVCalculatorHandler.py
@@ -1,16 +1,49 @@
+import csv, sys
+from Calculator import Calculator
+
 class CSVCalculatorHandler:
+    history_record = ["input_file,operation,result,error\n"] # a list that records every line of result 
+
     def __init__(self):
         pass
 
     def process_csvs(self, filenames, output_prefix="output"):
-        pass
+        # traverse each file, use enumerate to get both the index and the filename
+        for index, file in enumerate(filenames): 
+            # read file
+            with open(file,"r",newline="") as rf:
+                output_record = [["result","error"]] # output_record variable documents contents of the outputfile for each input file being read
+                
+                reader = csv.reader(rf)
+                for row in reader:
+                    # get the result tuple as the form (result, error code)
+                    result, error_code = Calculator.operation_handler(row.copy())
+                    # update output record
+                    if result is None:
+                        result = ""
+                    output_record.append([str(result),str(error_code)])
+                    # update history record
+                    operation = ",".join(row) # convert each row of csvfile into a string
+                    self.save_to_history(file, operation, result, error_code)
+
+            # generate output csv file
+            OutputFileName = f"{output_prefix}_{index}.csv"
+            with open(OutputFileName,"w",newline="") as wf:
+                writer = csv.writer(wf)
+                writer.writerows(output_record)
 
     def save_to_history(self, filename, operation, result, error_code):
-        pass
+        # this method add element to the class variable history_record
+        self.history_record.append(f'{filename},"{operation}",{result},{error_code}\n')
 
     def history_export(self, export_filename):
-        pass
+        # this method create a txt file from the class variable history_record
+        with open(export_filename,"w",newline="") as tf:
+            tf.writelines(self.history_record)
 
 
 if __name__ == '__main__':
-    print("write some code, your testing software will call this main function")
+    files = sys.argv[1:]
+    instance = CSVCalculatorHandler()
+    instance.process_csvs(files)
+    instance.history_export("history.txt")
--- Calculator.py
+++ Calculator.py
@@ -1,2 +1,97 @@
 class Calculator:
-    pass
+    @staticmethod
+    def convert_to_float(arr: list) -> list | None:
+        """
+        This method converts every element in arr into float, if an element is unconvertable, return None
+        else, return the converted list
+        """
+        converted_list = []
+        for v in arr:
+            try:
+                converted_list.append(float(v))
+            except Exception:
+                return None # the element is unconvertable, return None
+        return converted_list
+    
+    @staticmethod
+    def operation_handler(params: list) -> tuple[float | None, int]:
+        """
+        parameters: params, a row extracted from CSV files, contains a series of number followed by a operation
+        output: a tuple with 2 elements, which has the form: (result, error code)
+
+        This method takes a row of data from a CSV file, treats the last element as the operator, 
+        and calls the corresponding calculation method based on the operator. 
+        If the format is invalid or the operator is unrecognized, return the appropriate error code.
+        """
+
+        # if number of parameters < 3, then return Error 2
+        if len(params) < 3:
+            return (None,2) # Bad Number of Params
+        
+        operation = params.pop() # extract the last element
+        match operation:
+            case "add":
+                return Calculator.addition(params)
+            case "subtract":
+                return Calculator.subtraction(params)
+            case "multiply":
+                return Calculator.multiplication(params)
+            case "divide":
+                return Calculator.division(params)
+            case "exponentiate":
+                return Calculator.exponentiation(params)
+            case _: # if nothing matches, return Unknown Operator (3)
+                return (None, 3)
+            
+    @staticmethod
+    def addition(arr: list) -> tuple[float | None, int]:
+        operands = Calculator.convert_to_float(arr) # convert into floats
+        if operands is None:
+            return (None, 4) # at least one element cannot be converted to float, return Unknown Error
+        result = sum(operands)
+        return (result, 0)
+
+    @staticmethod
+    def subtraction(arr: list) -> tuple[float | None, int]:
+        operands = Calculator.convert_to_float(arr) # convert into floats
+        if operands is None:
+            return (None, 4) # at least one element cannot be converted to float, return Unknown Error
+        result = operands[0]
+        for v in range(1,len(operands)):
+            result -= operands[v]
+        return (result, 0)
+    
+    @staticmethod
+    def multiplication(arr: list) -> tuple[float | None, int]:
+        operands = Calculator.convert_to_float(arr) # convert into floats
+        if operands is None:
+            return (None, 4) # at least one element cannot be converted to float, return Unknown Error
+        result = operands[0]
+        for v in range(1,len(operands)):
+            result *= operands[v]
+        return (result,0)
+    
+    @staticmethod
+    def division(arr: list) -> tuple[float | None, int]:
+        operands = Calculator.convert_to_float(arr) # convert into floats
+        if operands is None:
+            return (None, 4) # at least one element cannot be converted to float, return Unknown Error
+        # check number of params
+        if len(operands) != 2:
+            return (None, 2)
+        # check if divide by 0
+        if operands[1] == 0:
+            return (None, 1)
+        result = operands[0]/operands[1]
+        return (result, 0)
+    
+    @staticmethod
+    def exponentiation(arr: list) -> tuple[float | None, int]:
+        operands = Calculator.convert_to_float(arr) # convert into floats
+        if operands is None:
+            return (None, 4) # at least one element cannot be converted to float, return Unknown Error
+        # check number of params
+        if len(operands) != 2:
+            return (None, 2)
+        result = operands[0]**operands[1]
+        return (result, 0)
